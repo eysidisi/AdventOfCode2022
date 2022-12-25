7bde64a1d0ec99ff550bc7252ebe1f253dd6fe22
ali elmas <aelmas@microsoft.com>
2022-12-25 20:41:28 +0000

    Finish day 9 part 2

--- AdventOfCode2022Solutions/Day09/Rope.cs
+++ AdventOfCode2022Solutions/Day09/Rope.cs
@@ -2,34 +2,77 @@
 {
     public class Rope
     {
-        public Position HeadPos { get; private set; } = new Position(0, 0);
-        public Position TailPos { get; private set; } = new Position(0, 0);
+        public Position HeadPos
+        {
+            get
+            {
+                return knotPositions.First();
+            }
+            private set
+            {
+                knotPositions[0] = value;
+            }
+        }
+
+        public Position TailPos
+        {
+            get
+            {
+                return knotPositions.Last();
+            }
+        }
+
+        public Position[] knotPositions;
+
         private HashSet<Position> visitedPositions = new()
         {
             new Position(0,0)
         };
 
-        public void MoveHead(string upMovement)
+        public Rope() : this(2) { }
+
+        public Rope(int numberOfKnots)
         {
-            string[] directionAndDistance = upMovement.Split(" ");
+            knotPositions = new Position[numberOfKnots];
+            for (int i = 0; i < numberOfKnots; i++)
+            {
+                knotPositions[i] = new Position(0, 0);
+            }
+        }
+
+        public void MoveHead(string movementString)
+        {
+            string[] directionAndDistance = movementString.Split(" ");
             string direction = directionAndDistance[0];
             int distance = int.Parse(directionAndDistance[1]);
 
             while (distance != 0)
             {
                 MoveHead(direction, 1);
-
-                if (DoesTailTouchHead() == false)
-                {
-                    MoveTail();
-                    UpdateVisitedPositions();
-                }
+                UpdateAllKnotsPositions();
 
                 distance--;
             }
         }
 
-        private void UpdateVisitedPositions()
+        private void UpdateAllKnotsPositions()
+        {
+            for (int i = 0; i < knotPositions.Length - 1; i++)
+            {
+                Position leadingNode = knotPositions[i];
+                ref Position followingNode = ref knotPositions[i + 1];
+
+                if (DoesFollowingTouchLeading(leadingNode, followingNode) == false)
+                    MoveFollowingToLeading(ref followingNode, leadingNode);
+                else
+                    break;
+
+                if (i == knotPositions.Length - 2)
+                    UpdateTailsVisitedPositions();
+            }
+        }
+
+        private void UpdateTailsVisitedPositions()
         {
             if (visitedPositions.Contains(TailPos) == false)
             {
@@ -37,36 +80,35 @@
             }
         }
 
-        private void MoveTail()
+        private void MoveFollowingToLeading(ref Position followingPos, Position leadingPos)
         {
-            int distanceToMoveInY = (HeadPos.Y - TailPos.Y) > 0 ? 1 : -1;
-            int distanceToMoveInX = (HeadPos.X - TailPos.X) > 0 ? 1 : -1;
+            int distanceToMoveInY = (leadingPos.Y - followingPos.Y) > 0 ? 1 : -1;
+            int distanceToMoveInX = (leadingPos.X - followingPos.X) > 0 ? 1 : -1;
 
-            if (AreTailHeadInTheSameRow())
+            if (ArePositionsInTheSameRow(followingPos, leadingPos))
             {
-                TailPos = new Position(TailPos.X, TailPos.Y + distanceToMoveInY);
+                followingPos = new Position(followingPos.X, followingPos.Y + distanceToMoveInY);
             }
 
-            else if (AreTailHeadInTheSameCol())
+            else if (ArePositionsInTheSameCol(followingPos, leadingPos))
             {
-                TailPos = new Position(TailPos.X + distanceToMoveInX, TailPos.Y);
+                followingPos = new Position(followingPos.X + distanceToMoveInX, followingPos.Y);
             }
 
             else
             {
-                TailPos = new Position(TailPos.X, TailPos.Y + distanceToMoveInY);
-                TailPos = new Position(TailPos.X + distanceToMoveInX, TailPos.Y);
+                followingPos = new Position(followingPos.X + distanceToMoveInX, followingPos.Y + distanceToMoveInY);
             }
         }
 
-        private bool AreTailHeadInTheSameCol()
+        private bool ArePositionsInTheSameCol(Position followingPos, Position leadingPos)
         {
-            return HeadPos.Y - TailPos.Y == 0;
+            return followingPos.Y - leadingPos.Y == 0;
         }
 
-        private bool AreTailHeadInTheSameRow()
+        private bool ArePositionsInTheSameRow(Position followingPos, Position leadingPos)
         {
-            return HeadPos.X - TailPos.X == 0;
+            return followingPos.X - leadingPos.X == 0;
         }
 
         public int NumberOfPositionsTailVisited()
@@ -74,10 +116,10 @@
             return visitedPositions.Count;
         }
 
-        private bool DoesTailTouchHead()
+        private bool DoesFollowingTouchLeading(Position Pos1, Position Pos2)
         {
-            return Math.Abs(TailPos.X - HeadPos.X) < 2 &&
-                Math.Abs(TailPos.Y - HeadPos.Y) < 2;
+            return Math.Abs(Pos1.X - Pos2.X) < 2 &&
+                Math.Abs(Pos1.Y - Pos2.Y) < 2;
         }
 
         private void MoveHead(string direction, int distance)
@@ -105,6 +147,5 @@
             else
                 throw new ArgumentException("Undefined direction!");
         }
-
     }
 }
---
+++ AdventOfCode2022Tests/Day09/Part2.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+
+namespace AdventOfCode2022Tests.Day09
+{
+    public class Part2
+    {
+        [Fact]
+        public void RopeHasMoreThanOneKnots()
+        {
+            int numberOfKnots = 2;
+            Rope rope = new(numberOfKnots);
+        }
+
+        [Fact]
+        public void RopeControlsTenKnots()
+        {
+            Rope rope = new(10);
+
+            rope.MoveHead("R 5");
+
+            Assert.Equal(new Position(5, 0), rope.knotPositions[0]);
+            Assert.Equal(new Position(4, 0), rope.knotPositions[1]);
+            Assert.Equal(new Position(3, 0), rope.knotPositions[2]);
+            Assert.Equal(new Position(2, 0), rope.knotPositions[3]);
+            Assert.Equal(new Position(1, 0), rope.knotPositions[4]);
+            Assert.Equal(new Position(0, 0), rope.knotPositions[5]);
+            Assert.Equal(new Position(0, 0), rope.knotPositions[6]);
+
+            rope.MoveHead("U 8");
+            Assert.Equal(new Position(5, 8), rope.knotPositions[0]);
+            Assert.Equal(new Position(5, 7), rope.knotPositions[1]);
+            Assert.Equal(new Position(5, 6), rope.knotPositions[2]);
+            Assert.Equal(new Position(5, 5), rope.knotPositions[3]);
+            Assert.Equal(new Position(5, 4), rope.knotPositions[4]);
+            Assert.Equal(new Position(4, 4), rope.knotPositions[5]);
+            Assert.Equal(new Position(3, 3), rope.knotPositions[6]);
+            Assert.Equal(new Position(2, 2), rope.knotPositions[7]);
+            Assert.Equal(new Position(1, 1), rope.knotPositions[8]);
+            Assert.Equal(new Position(0, 0), rope.knotPositions[9]);
+
+            rope.MoveHead("L 8");
+            Assert.Equal(new Position(-3, 8), rope.knotPositions[0]);
+            Assert.Equal(new Position(-2, 8), rope.knotPositions[1]);
+            Assert.Equal(new Position(-1, 8), rope.knotPositions[2]);
+            Assert.Equal(new Position(0, 8), rope.knotPositions[3]);
+            Assert.Equal(new Position(1, 8), rope.knotPositions[4]);
+            Assert.Equal(new Position(1, 7), rope.knotPositions[5]);
+            Assert.Equal(new Position(1, 6), rope.knotPositions[6]);
+            Assert.Equal(new Position(1, 5), rope.knotPositions[7]);
+            Assert.Equal(new Position(1, 4), rope.knotPositions[8]);
+            Assert.Equal(new Position(1, 3), rope.knotPositions[9]);
+
+            rope.MoveHead("D 3");
+            rope.MoveHead("R 17");
+            rope.MoveHead("D 10");
+            rope.MoveHead("L 25");
+            rope.MoveHead("U 20");
+
+            int expectedNumberOfVisitedPos = 36;
+
+            Assert.Equal(expectedNumberOfVisitedPos, rope.NumberOfPositionsTailVisited());
+        }
+
+        [Fact]
+        public void ExerciseSolution()
+        {
+            string[] input = File.ReadAllLines(@"TestFiles/Day09/ExerciseInput.txt");
+            Rope rope = new(10);
+
+            foreach (string item in input)
+            {
+                rope.MoveHead(item);
+            }
+
+            int expectedNumOfPositionsVisited = 5878;
+
+            Assert.Equal(expectedNumOfPositionsVisited, rope.NumberOfPositionsTailVisited());
+        }
+
+    }
+}

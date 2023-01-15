0015e3a38e9e41984a8a4a1d8c045b55e65cc015
ali elmas <aelmas@microsoft.com>
2023-01-15 11:43:58 +0000

    Finish day 14 part 1

---
+++ AdventOfCode2022Solutions/Day14/Cave.cs
@@ -0,0 +1,188 @@
+﻿using System.Runtime.Serialization;
+
+namespace AdventOfCode2022Tests.Day14
+{
+    public class Cave
+    {
+        HashSet<Coord> rocks = new();
+        HashSet<Coord> sands = new();
+
+        public bool HasRock(int xCoord, int yCoord)
+        {
+            return rocks.Contains(new Coord(xCoord, yCoord));
+        }
+
+        public void ParseInput(string input)
+        {
+            input = input.Replace(" ", "");
+
+            string[] lines = input.Split("\r\n");
+
+            foreach (string line in lines)
+            {
+                ParseLineAndAddRocks(line);
+            }
+        }
+
+        private void ParseLineAndAddRocks(string input)
+        {
+            string[] coordinatePairs = input.Split("->");
+
+            for (int i = 0; i < coordinatePairs.Length - 1; i++)
+            {
+                Coord startingCoord = CreateCoordinate(coordinatePairs[i]);
+                Coord endingCoord = CreateCoordinate(coordinatePairs[i + 1]);
+
+                FillBetweenTheseCoords(startingCoord, endingCoord);
+            }
+        }
+
+        private void FillBetweenTheseCoords(Coord startingCoord, Coord endingCoord)
+        {
+            if (startingCoord.X == endingCoord.X)
+            {
+                for (int yCoord = Math.Min(startingCoord.Y, endingCoord.Y);
+                    yCoord <= Math.Max(startingCoord.Y, endingCoord.Y);
+                    yCoord++)
+                {
+                    rocks.Add(new Coord(startingCoord.X, yCoord));
+                }
+            }
+
+            else
+            {
+                for (int xCoord = Math.Min(startingCoord.X, endingCoord.X);
+                    xCoord <= Math.Max(startingCoord.X, endingCoord.X);
+                    xCoord++)
+                {
+                    rocks.Add(new Coord(xCoord, startingCoord.Y));
+                }
+            }
+        }
+
+        private Coord CreateCoordinate(string coord)
+        {
+            int x = int.Parse(coord.Split(",")[0]);
+            int y = int.Parse(coord.Split(",")[1]);
+
+            return new Coord(x, y);
+        }
+
+        public void DropOneSand()
+        {
+            Coord sandCurrentCoord = new(500, 0);
+            int maxDepth = rocks.Max(r => r.Y);
+
+            while (true)
+            {
+                Coord downOne = new(sandCurrentCoord.X, sandCurrentCoord.Y + 1);
+                Coord leftOneDownOne = new(sandCurrentCoord.X - 1, sandCurrentCoord.Y + 1);
+                Coord rightOneDownOne = new(sandCurrentCoord.X + 1, sandCurrentCoord.Y + 1);
+
+                Coord sandNextCoord;
+
+                if (sandCurrentCoord.Y >= maxDepth)
+                {
+                    throw new SandException("Sands started going into the void");
+                }
+
+                if (rocks.Contains(downOne) == false && sands.Contains(downOne) == false)
+                {
+                    sandNextCoord = downOne;
+                }
+
+                else if (rocks.Contains(leftOneDownOne) == false && sands.Contains(leftOneDownOne) == false)
+                {
+                    sandNextCoord = leftOneDownOne;
+                }
+
+                else if (rocks.Contains(rightOneDownOne) == false && sands.Contains(rightOneDownOne) == false)
+                {
+                    sandNextCoord = rightOneDownOne;
+                }
+
+                else
+                    break;
+
+                sandCurrentCoord = sandNextCoord;
+            }
+
+            sands.Add(sandCurrentCoord);
+        }
+
+        public bool HasSand(int x, int y)
+        {
+            return sands.Contains(new Coord(x, y));
+        }
+
+        public int FindNumberOfSandsThatCanRest()
+        {
+            int numberOfSands = 0;
+
+            while (true)
+            {
+                try
+                {
+                    DropOneSand();
+                    numberOfSands++;
+                }
+                catch (SandException)
+                {
+                    break;
+                }
+            }
+            return numberOfSands;
+        }
+    }
+
+    public class Coord
+    {
+        public Coord(int xCoord, int yCoord)
+        {
+            X = xCoord;
+            Y = yCoord;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Coord other = (Coord)(obj);
+
+            return X == other.X && Y == other.Y;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+    }
+}
+
+[Serializable]
+internal class SandException : Exception
+{
+    public SandException()
+    {
+    }
+
+    public SandException(string? message) : base(message)
+    {
+    }
+
+    public SandException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected SandException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
---
+++ AdventOfCode2022Tests/Day14/Part1.cs
@@ -0,0 +1,158 @@
+﻿using Xunit;
+
+namespace AdventOfCode2022Tests.Day14
+{
+    public partial class Part1
+    {
+        [Fact]
+        public void ParseOneLineInput()
+        {
+            string input = "498,4-> 498,6";
+
+            Cave cave = new();
+
+            cave.ParseInput(input);
+
+            Assert.True(cave.HasRock(498, 4));
+            Assert.True(cave.HasRock(498, 5));
+            Assert.True(cave.HasRock(498, 6));
+            Assert.False(cave.HasRock(498, 7));
+            Assert.False(cave.HasRock(498, 3));
+            Assert.False(cave.HasRock(499, 5));
+        }
+
+        [Fact]
+        public void ParseOneLineMoreThanTwoCoordinates()
+        {
+            string input = "498,4-> 498,3-> 495,3";
+
+            Cave cave = new();
+
+            cave.ParseInput(input);
+
+            Assert.True(cave.HasRock(498, 4));
+            Assert.True(cave.HasRock(498, 3));
+            Assert.True(cave.HasRock(497, 3));
+            Assert.True(cave.HasRock(496, 3));
+            Assert.True(cave.HasRock(495, 3));
+
+
+            Assert.False(cave.HasRock(494, 3));
+            Assert.False(cave.HasRock(495, 2));
+        }
+
+        [Fact]
+        public void ParseTwoLinesInput()
+        {
+            string input = "498,4 -> 498,6 -> 496,6\r\n" +
+                "503,4 -> 502,4 -> 502,9 -> 494,9";
+
+            Cave cave = new();
+            cave.ParseInput(input);
+
+            Assert.True(cave.HasRock(498, 4));
+            Assert.True(cave.HasRock(498, 5));
+            Assert.True(cave.HasRock(498, 6));
+            Assert.True(cave.HasRock(497, 6));
+            Assert.True(cave.HasRock(496, 6));
+
+            Assert.True(cave.HasRock(503, 4));
+            Assert.True(cave.HasRock(502, 4));
+            Assert.True(cave.HasRock(502, 5));
+            Assert.True(cave.HasRock(502, 6));
+            Assert.True(cave.HasRock(502, 7));
+            Assert.True(cave.HasRock(502, 8));
+            Assert.True(cave.HasRock(502, 9));
+
+            Assert.True(cave.HasRock(501, 9));
+            Assert.True(cave.HasRock(500, 9));
+            Assert.True(cave.HasRock(499, 9));
+            Assert.True(cave.HasRock(498, 9));
+            Assert.True(cave.HasRock(497, 9));
+            Assert.True(cave.HasRock(496, 9));
+            Assert.True(cave.HasRock(495, 9));
+            Assert.True(cave.HasRock(494, 9));
+        }
+
+        [Fact]
+        public void SandGoesDown()
+        {
+            string input = "498,4 -> 498,6 -> 496,6\r\n" +
+                "503,4 -> 502,4 -> 502,9 -> 494,9";
+
+            Cave cave = new();
+            cave.ParseInput(input);
+
+            int numOfSands = 24;
+
+            while (numOfSands > 0)
+            {
+                cave.DropOneSand();
+                numOfSands--;
+            }
+
+            Assert.True(cave.HasSand(500, 2));
+
+            Assert.True(cave.HasSand(500, 3));
+            Assert.True(cave.HasSand(501, 3));
+            Assert.True(cave.HasSand(499, 3));
+
+            Assert.True(cave.HasSand(500, 4));
+            Assert.True(cave.HasSand(501, 4));
+            Assert.True(cave.HasSand(499, 4));
+
+            Assert.True(cave.HasSand(497, 5));
+            Assert.True(cave.HasSand(499, 5));
+            Assert.True(cave.HasSand(500, 5));
+            Assert.True(cave.HasSand(501, 5));
+
+            Assert.True(cave.HasSand(499, 6));
+            Assert.True(cave.HasSand(500, 6));
+            Assert.True(cave.HasSand(501, 6));
+
+            Assert.True(cave.HasSand(498, 7));
+            Assert.True(cave.HasSand(499, 7));
+            Assert.True(cave.HasSand(500, 7));
+            Assert.True(cave.HasSand(501, 7));
+
+            Assert.True(cave.HasSand(495, 8));
+            Assert.True(cave.HasSand(497, 8));
+            Assert.True(cave.HasSand(498, 8));
+            Assert.True(cave.HasSand(499, 8));
+            Assert.True(cave.HasSand(500, 8));
+            Assert.True(cave.HasSand(501, 8));
+        }
+
+        [Fact]
+        public void SandGoesIntoTheVoid()
+        {
+            string input = "498,4 -> 498,6 -> 496,6\r\n" +
+                "503,4 -> 502,4 -> 502,9 -> 494,9";
+
+            Cave cave = new();
+            cave.ParseInput(input);
+
+            int expedtedNumberOfSands = 24;
+
+            int actualNumberOfSandsThanCanRest = cave.FindNumberOfSandsThatCanRest();
+
+            Assert.Equal(expedtedNumberOfSands, actualNumberOfSandsThanCanRest);
+        }
+
+        [Fact]
+        public void ExerciseSolution()
+        {
+            string inputPath = @"TestFiles/Day14/ExerciseInput.txt";
+            string input = File.ReadAllText(inputPath);
+
+            Cave cave = new();
+            cave.ParseInput(input);
+
+            int expedtedNumberOfSands = 674;
+
+            int actualNumberOfSandsThanCanRest = cave.FindNumberOfSandsThatCanRest();
+
+            Assert.Equal(expedtedNumberOfSands, actualNumberOfSandsThanCanRest);
+        }
+    }
+}

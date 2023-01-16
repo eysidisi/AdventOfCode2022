37837bf1396c74d2daa385882fa13de79f9512b7
ali elmas <aelmas@microsoft.com>
2023-01-16 21:09:21 +0000

    Solve day 15 Part 1

---
+++ AdventOfCode2022Solutions/Day15/Zone.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022Tests.Day15
+{
+    public class Zone
+    {
+        Dictionary<(int x, int y), (int x, int y)> sensorsToBeacons = new();
+
+        public Zone(string input)
+        {
+            string[] lines = input.Split("\r\n");
+
+            foreach (string line in lines)
+            {
+                ParseSensorsAndBeacons(line);
+            }
+        }
+
+        private void ParseSensorsAndBeacons(string input)
+        {
+            Regex reg = new("(-?\\d+)");
+            MatchCollection matches = reg.Matches(input);
+
+            int sensorX = int.Parse(matches[0].Value);
+            int sensorY = int.Parse(matches[1].Value);
+
+            int beaconX = int.Parse(matches[2].Value);
+            int beaconY = int.Parse(matches[3].Value);
+            sensorsToBeacons.Add((sensorX, sensorY), (beaconX, beaconY));
+        }
+
+        public bool HasBeaconAt(int x, int y)
+        {
+            return sensorsToBeacons.ContainsValue((x, y));
+        }
+
+        public bool HasSensorAt(int x, int y)
+        {
+            return sensorsToBeacons.ContainsKey((x, y));
+        }
+
+        public int HowManyCellsAreBlockedInRow(int blockedRowNumber)
+        {
+            HashSet<(int, int)> àllBlockedCells = new();
+
+            foreach (KeyValuePair<(int x, int y), (int x, int y)> sensorToBeacon in sensorsToBeacons)
+            {
+                (int x, int y) sensor = sensorToBeacon.Key;
+                (int x, int y) beacon = sensorToBeacon.Value;
+                int sensorRadius = CalculateSensorRadius(sensor, beacon);
+
+                bool sensorRangeReachesRow = sensorRadius >= sensor.y - blockedRowNumber;
+
+                if (sensorRangeReachesRow)
+                {
+                    HashSet<(int, int)> blockedCells = FindBlockedCellsInRowBecauseOfThisPair(blockedRowNumber, sensorToBeacon);
+                    àllBlockedCells.UnionWith(blockedCells);
+                }
+            }
+
+            àllBlockedCells.RemoveWhere(b => sensorsToBeacons.ContainsKey(b) || sensorsToBeacons.ContainsValue(b));
+
+            return àllBlockedCells.Count();
+        }
+
+        private static int CalculateSensorRadius((int x, int y) sensor, (int x, int y) beacon)
+        {
+            return Math.Abs(sensor.x - beacon.x) + Math.Abs(sensor.y - beacon.y);
+        }
+
+        private HashSet<(int, int)> FindBlockedCellsInRowBecauseOfThisPair(int rowIndex, KeyValuePair<(int x, int y), (int x, int y)> sensorToBeacon)
+        {
+            (int x, int y) sensor = sensorToBeacon.Key;
+            (int x, int y) beacon = sensorToBeacon.Value;
+
+            int sensorRadius = CalculateSensorRadius(sensor, beacon);
+
+            int xDelta = sensorRadius - Math.Abs(sensor.y - rowIndex);
+            HashSet<(int, int)> blockedCells = new();
+
+            for (int x = sensor.x - xDelta; x <= sensor.x + xDelta; x++)
+            {
+                blockedCells.Add((x, rowIndex));
+            }
+
+            return blockedCells;
+        }
+    }
+}
---
+++ AdventOfCode2022Tests/Day15/Part1.cs
@@ -0,0 +1,82 @@
+﻿using Xunit;
+
+namespace AdventOfCode2022Tests.Day15
+{
+    public class Part1
+    {
+        [Fact]
+        public void ParseInput()
+        {
+            string input = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15";
+            Zone zone = new(input);
+
+            Assert.True(zone.HasSensorAt(2, 18));
+            Assert.True(zone.HasBeaconAt(-2, 15));
+
+            Assert.False(zone.HasSensorAt(-2, 15));
+            Assert.False(zone.HasBeaconAt(2, 18));
+        }
+
+        [Fact]
+        public void ParseMultipleLineInput()
+        {
+            string input = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\r\n" +
+                "Sensor at x=9, y=16: closest beacon is at x=10, y=16";
+
+            Zone zone = new(input);
+
+            Assert.True(zone.HasSensorAt(2, 18));
+            Assert.True(zone.HasBeaconAt(-2, 15));
+            Assert.True(zone.HasSensorAt(9, 16));
+            Assert.True(zone.HasBeaconAt(10, 16));
+        }
+
+        [Fact]
+        public void CalculateNumberOfBlockedCellsOnePair()
+        {
+            string input = "Sensor at x=2, y=5: closest beacon is at x=0, y=5";
+
+            Zone zone = new(input);
+
+            Assert.Equal(3, zone.HowManyCellsAreBlockedInRow(5));
+        }
+
+        [Fact]
+        public void CalculateNumberOfBlockedCellsMultiplePairs()
+        {
+            string input = "Sensor at x=2, y=0: closest beacon is at x=3, y=-1\r\n" +
+            "Sensor at x=7, y=2: closest beacon is at x=7, y=-1\r\n" +
+                "Sensor at x=12, y=0: closest beacon is at x=16, y=0";
+
+            Zone zone = new(input);
+
+            Assert.Equal(13, zone.HowManyCellsAreBlockedInRow(0));
+            Assert.Equal(14, zone.HowManyCellsAreBlockedInRow(1));
+            Assert.Equal(11, zone.HowManyCellsAreBlockedInRow(2));
+        }
+
+        [Fact]
+        public void LargeExample()
+        {
+            string filePath = @"TestFiles/Day15/LargeExample.txt";
+            string input = File.ReadAllText(filePath);
+
+            Zone zone = new(input);
+
+            Assert.Equal(25, zone.HowManyCellsAreBlockedInRow(9));
+            Assert.Equal(26, zone.HowManyCellsAreBlockedInRow(10));
+            Assert.Equal(27, zone.HowManyCellsAreBlockedInRow(11));
+        }
+
+        [Fact]
+        public void ExerciseSolution()
+        {
+            string filePath = @"TestFiles/Day15/ExerciseInput.txt";
+            string input = File.ReadAllText(filePath);
+
+            Zone zone = new(input);
+
+            Assert.Equal(5335787, zone.HowManyCellsAreBlockedInRow(2000000));
+        }
+    }
+}
